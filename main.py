e966e183d840f7cf9b10ddf197eeb77d145263d6
Caleb Lawson <caleb.g.lawson@gmail.com>
2020-12-29 16:42:26 -0500

    main and template config

---
+++ main.py
@@ -0,0 +1,77 @@
+'''
+This file contains the necessary components to mass-unblock / mass-unmute users.
+'''
+
+import json
+
+import typer
+import tweepy
+
+
+APP = typer.Typer()
+
+
+def make_config():
+    '''
+    Load the config from a file.
+    '''
+    config = open('./config.json')
+    return json.load(config)
+
+
+def make_api(config):
+    '''
+    Make a Tweepy api object.
+    '''
+    auth = tweepy.OAuthHandler(
+        config.get('consumer_key'),
+        config.get('consumer_secret')
+    )
+
+    auth.set_access_token(
+        config.get('access_token'),
+        config.get('access_token_secret')
+    )
+
+    api = tweepy.API(
+        auth,
+        wait_on_rate_limit=True
+    )
+
+    return api
+
+
+@APP.command
+def unblock():
+    '''
+    Unblock all the users the authenticated user is blocking.
+    '''
+    api = make_api(make_config())
+
+    blocked_users = api.blocks_ids()
+
+    with typer.progressbar(blocked_users, label="Unblocking") as blocked_users_progress:
+        for user in blocked_users_progress:
+            api.destroy_block(user)
+
+    typer.echo(f'Unblocked {len(blocked_users)} users')
+
+
+@APP.command
+def unmute():
+    '''
+    Unmute all the users the authenticated user is muting.
+    '''
+    api = make_api(make_config())
+
+    muted_users = api.mutes_ids()
+
+    with typer.progressbar(muted_users, label="Unmuting") as muted_users_progress:
+        for user in muted_users_progress:
+            api.destroy_mute(user)
+
+    typer.echo(f'Unmuted {len(muted_users)} users')
+
+
+if __name__ == "__main__":
+    APP()
